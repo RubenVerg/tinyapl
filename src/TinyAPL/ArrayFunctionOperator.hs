de76e28713d0d8183cedf4cba3e4eed976784da4
Madeline Vergani <RubenVerg@outlook.com>
2024-04-05 15:15:19 +0200

    Show vector of `Character`s as a string instead of an array

--- src/TinyAPL/ArrayFunctionOperator.hs
+++ src/TinyAPL/ArrayFunctionOperator.hs
@@ -144,9 +144,10 @@ instance Show ScalarValue where
 
 -- We'll implement proper array formatting later.
 instance Show Array where
-  show (Array [] [s]) = show s
-  show (Array [_] xs) = [fst G.vector] ++ intercalate [' ', G.separator, ' '] (show <$> xs) ++ [snd G.vector]
-  show arr            = [fst G.highRank] ++ intercalate [' ', G.separator, ' '] (show <$> majorCells arr) ++ [snd G.highRank]
+  show (Array [] [s])                                                    = show s
+  show (Array [_] xs) | all (\case (Character _) -> True; _ -> False) xs = xs >>= show
+                      | otherwise                                        = [fst G.vector] ++ intercalate [' ', G.separator, ' '] (show <$> xs) ++ [snd G.vector]
+  show arr                                                               = [fst G.highRank] ++ intercalate [' ', G.separator, ' '] (show <$> majorCells arr) ++ [snd G.highRank]
 
 -- * Conversions
 
